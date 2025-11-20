552e872cc6dd3ef7cc60a5229df3f78a3c0b5df2
Aissatou Coulibaly <coulibalyaissatou37@gmail.com>
2025-11-20 16:10:16 +0000

    Ajout du fichier constantes.dart avec la classe Constantes et correction des imports

---
+++ lib/constantes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Constantes {
+  static const String logoPath = 'assets/images/logo_suguconnect.png';
+  static const String landingWomenPath = 'assets/images/landing_women.png';
+
+  static const Color primaryOrange = Color(0xFFFF6B35);
+  static const Color secondaryGreen = Color(0xFF4CAF50);
+  static const Color backgroundColor = Color(0xFFFEE8E3);
+  static const Color beigeBackground = Color(0xFFF5F5DC);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF757575);
+
+  static const Color errorColor = Color(0xFFE53935);
+  static const Color successColor = Color(0xFF4CAF50);
+
+  static const Color grey200 = Color(0xFFEEEEEE);
+  static const Color grey300 = Color(0xFFE0E0E0);
+  static const Color grey500 = Color(0xFF9E9E9E);
+  static const Color grey600 = Color(0xFF757575);
+  static const Color grey100 = Color(0xFFF5F5F5);
+}
+
